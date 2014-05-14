735ee0b01cb88228f89e47d3df75e9e173103572
BlustrawberRi <lollymausi@freenet.de>
2014-05-14 16:16:07 +0200

    Template: Übersetzung (deutsch) hinzugefügt Anpassungen

--- template_onepage/js/onepage.js
+++ template_onepage/js/onepage.js
@@ -1,6 +1,7 @@
-var DID_SCROLL = false;
-var CURRENTLY_SCROLLING = false; //damit nicht zu viele Events überlappen
-var ITEMS = null;
+var DID_SCROLL = false; //um nicht zu viel Performance zu ziehen wird es in Intervallen behandelt
+var CURRENTLY_SSCROLLING = false; //damit nicht zu viele SoftScrollEvents ï¿½berlappen
+var ITEMS = null; //Die Kategorien und deren Artikel
+var STOP_AUTO_SCROLL = false;
 
 window.onscroll = function(){ 
 	DID_SCROLL = true; 
@@ -8,11 +9,14 @@ window.onscroll = function(){
 
 setInterval(function(){if (DID_SCROLL)writeText();  DID_SCROLL=false;},200);
 
-document.onkeydown = function(event){
+document.onmousedown = stopAutoScrolling;
+document.onmousewheel = stopAutoScrolling;
+document.onkeydown = function(event){ //scrollen auf Tastendruck
+	
 	if(event.which == 38) {//up
 		//alert("Hoch");
 		softScrollToPage("prev");
-		event.stopImmediatePropagation(); //stoppt normale Ausführung
+		event.stopImmediatePropagation(); //stoppt normale Ausfï¿½hrung
 		event.preventDefault();
 	}
 	else if(event.which == 40) {//down
@@ -24,7 +28,15 @@ document.onkeydown = function(event){
 	//alert(event.target); -> zB body
 };
 
-function softScrollToPage(dir) {//gibt scrollwert der letzten/nächsten elementes mit Klasse "page"
+function stopAutoScrolling(){ //stopt Autoscrollen
+	if(CURRENTLY_SSCROLLING) {
+		STOP_AUTO_SCROLL = true;
+		CURRENTLY_SSCROLLING = false;
+	}
+}
+
+function softScrollToPage(dir) {//gibt scrollwert der letzten/nï¿½chsten elementes mit Klasse "page"
+	STOP_AUTO_SCROLL = false; //Automatisches scrollen wird initialisiert
 	var start = getScrollXY().y, end=start, tmp={pos:null, i:null};
 	var eles = document.getElementsByClassName("page");
 	for (var i=0; i<eles.length; i++) {
@@ -33,14 +45,14 @@ function softScrollToPage(dir) {//gibt scrollwert der letzten/nächsten elementes
 			pos += par.offsetTop;
 			par = par.offsetParent;
 		}
-		if (dir =="next" && start<pos) {//gibt es ein nächstes Element
-			if (tmp.pos==null || pos<tmp.pos) {//neues element ist näher an start
+		if (dir =="next" && start<pos) {//gibt es ein nï¿½chstes Element
+			if (tmp.pos==null || pos<tmp.pos) {//neues element ist nï¿½her an start
 				tmp.pos = pos; //merken
 				tmp.i = i;
 			}
 		}
 		else if (dir =="prev" && start>pos) {//gibt es ein vorheriges Element
-			if (tmp.pos==null || pos>tmp.pos) {//neues element ist näher an start
+			if (tmp.pos==null || pos>tmp.pos) {//neues element ist nï¿½her an start
 				tmp.pos = pos; //merken
 				tmp.i = i;
 			}
@@ -48,12 +60,13 @@ function softScrollToPage(dir) {//gibt scrollwert der letzten/nächsten elementes
 	}
 	if (tmp.pos != null) end = tmp.pos;
 	//if (eles[tmp.i].id != null) ; //gibt es eine id dann mach was damit
-	if (!CURRENTLY_SCROLLING) softscroll(start, end);
+	if (!CURRENTLY_SSCROLLING) softscroll(start, end); //wenn nicht bereits gescrollt wird
 	return end;
 }
 
 
 function softscrollTo(anchor) {
+	STOP_AUTO_SCROLL = false; //Automatisches scrollen wird initialisiert
 	//get momentane scrollposition
 	var start = getScrollXY().y, end = 0;
 	//hole position des anchor
@@ -67,20 +80,20 @@ function softscrollTo(anchor) {
 		ele = ele.offsetParent;
 	}
 	//softscroll(stpos, curpos, despos);
-	if (end != null && !CURRENTLY_SCROLLING) softscroll(start, end);
+	if (end != null && !CURRENTLY_SSCROLLING) softscroll(start, end);
 }
 
 function softscroll(from, to, now, dir) {
-	CURRENTLY_SCROLLING = true;
+	CURRENTLY_SSCROLLING = true;
 	if(typeof now == 'undefined'){
 		to = to-from;
 		from = 0;
 		now = 0;
 		if (to < 0) {
 			to = to * -1;
-			dir = -1; //rückwärts scrollen
+			dir = -1; //rï¿½ckwï¿½rts scrollen
 		}
-		else dir = 1; //vorwärts scrollen
+		else dir = 1; //vorwï¿½rts scrollen
 	}
 	
 	var max = 40;
@@ -89,7 +102,7 @@ function softscroll(from, to, now, dir) {
 	/*if ((now-from) <= sudist/2) var su = Math.round((max/sudist/2)*now); //Beschleunigungsfunktion in Abh. von Distanz
 	else if ((now-from) <= sudist) var su = Math.round(-(max/sudist/2)*time+(2*max));
 	else var su=0;*/
-	if (to-from < sudist*2) sudist = (to-from)/2; //falls scrollen kürzer als Beschleunigung
+	if (to-from < sudist*2) sudist = (to-from)/2; //falls scrollen kï¿½rzer als Beschleunigung
 	if ((now-from) <= sudist) {
 		speed = Math.round((max/sudist)*(now-from)+0.5); //aufrunden
 	}
@@ -97,11 +110,11 @@ function softscroll(from, to, now, dir) {
 		speed = Math.round(-(max/sudist)*(now-from)+(max/sudist*to)+0.5);
 	}
 	else speed = max;
-    if (!NEW_SCROLL && now<to) {
+    if (!STOP_AUTO_SCROLL && now<to) {
     	window.scrollBy(0,speed*dir);//Richtung beachten
     	setTimeout(function(){softscroll(from, to, now+speed, dir);},50);
     }
-    else CURRENTLY_SCROLLING = false; //zuende
+    else CURRENTLY_SSCROLLING = false; //zuende
     
  
 }
@@ -128,17 +141,17 @@ function setItems(items) { ITEMS = items; }
 function writeText(){ //stellt auch current item ein
 	var scroll = getScrollXY().y, topoffset=10, tmp={pos:null, id:null};
 	var eles = document.getElementsByClassName("page"); //holt sich divs mit inhalt
-	var no_text = true; //man liegt außerhalb des Textbereich
+	var no_text = true; //man liegt auï¿½erhalb des Textbereich
 	for (var i=0; i<eles.length; i++) {
 		var pos = 0, par=eles[i];
 		while (par != "html" && par != null) {//finde scrollposition
 			pos += par.offsetTop;
 			par = par.offsetParent;
 		}
-		if (scroll-pos > 0) {//div-position liegt auf/über scrollposition
-			if (tmp.pos==null || scroll-pos<scroll-tmp.pos) {//neues element ist näher an der scrollposition
+		if (scroll-pos >= 0) {//div-position liegt auf/ï¿½ber scrollposition
+			if (tmp.pos==null || scroll-pos<scroll-tmp.pos) {//neues element ist nï¿½her an der scrollposition
 				tmp.pos = pos; //merken
-				tmp.id = eles[i].id; //id übergeben
+				tmp.id = eles[i].id; //id ï¿½bergeben
 				no_text = false;
 			}
 		}
@@ -163,4 +176,4 @@ function writeText(){ //stellt auch current item ein
 		}
 	}
 	document.all.text.innerHTML = text;
-}	//.getBoundingClientRect().top 
+}
