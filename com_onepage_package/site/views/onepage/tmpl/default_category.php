3d3bf994013b79dca0cab1f8c015cc42677e46cd
Ri <lollymausi@freenet.de>
2014-06-12 10:20:34 +0200

    Spezialseiten verändert

--- com_onepage_package/site/views/onepage/tmpl/default_category.php
+++ com_onepage_package/site/views/onepage/tmpl/default_category.php
@@ -4,25 +4,86 @@ defined('_JEXEC') or die;
 
 $cat = $this->curcat; //lade momentane Kategorie
 
-if (preg_match("#show_team#", $cat->text) != 1) { //überprüfe auf Spezialseite
-
-?>
-	
-	<div class="category" id="<?php echo "category " . $cat->alias; ?>" >
-	<?php 
-	//--- Kategorieinhalt ausgeben (falls Bild vorhanden ist) 
-	if ($cat->images != null) {  ?> 
-		<div class="page" id="<?php echo $cat->alias . " _cont"; ?>">
-			<h2><?php echo $cat->cattitle; ?></h2>
-			<img  class="artimg" src="<?php echo $cat->images[0]; ?>" alt="" />
+if (preg_match("#{show_.*}#", $cat->text) != 1) { //überprüfe auf Spezialseite
+//---------------------- Anzeige für normale Seite ----------
+	?>
+		
+		<div class="category" id="<?php echo "category " . $cat->alias; ?>" >
+		<?php 
+		//--- Kategorieinhalt ausgeben (falls Bild vorhanden ist) 
+		if ($cat->images != null) {  ?> 
+			<div class="page" id="<?php echo $cat->alias . " _cont"; ?>">
+				<h2><?php echo $cat->cattitle; ?></h2>
+				<img  class="artimg" src="<?php echo $cat->images[0]; ?>" alt="" />
+			</div>
+		<?php }
+		
+		//--- Ausgabe der zugehörigen Artikel (wenn es welche gibt)
+		if ($cat->articles != null) {
+			foreach ($cat->articles as $art) {
+				//--- Google maps anstadt Bild 
+			
+				if ($art->images != null) { //nur ausgeben wenn Bild vorhanden ist	?>
+					<div class="page" id="<?php echo $cat->alias . ' ' . $art->alias; ?>">
+						<h3><?php echo $art->title; ?></h3>
+						<img  class="artimg" src="<?php echo $art->images[0]; ?>" alt="" />
+					</div>
+			<?php }
+			} 
+		}
+		
+		
+		//--- Ausgabe der zugehörigen Unterkategorien
+		foreach ($this->categories as $ucat) {
+			if ($ucat->catparent == $cat->catid ) {
+				if (preg_match("#{show_map}#", $ucat->text) == 1) {//ist googleMaps
+					$this->curcat = $ucat;
+					echo $this->loadTemplate('category');
+				}            
+				if ( $ucat->articles != null) { //nur wenn die Unterkategorie irgendeinen Inhalt hat
+					$content = false;
+					if ($ucat->images != null) { $content = true;} //hat selber Inhalt
+					foreach ($ucat->articles as $uart) { //prüfe Artikel auf Inhalt
+						if ($uart->images != null) { $content = true;}
+					}
+					if ($content == true) {
+						$this->curcat = $ucat;
+						echo $this->loadTemplate('category');
+					}
+				}
+			}
+		}?>
 		</div>
 	<?php }
-	
-	//--- Ausgabe der zugehörigen Artikel (wenn es welche gibt)
+		
+
+//---------------------- Anzeige für Ansprechpartner Seite ----------
+else if (preg_match("#{show_team}#", $cat->text) == 1) { //überprüfe auf Spezialseite
+?>
+
+<div class="category staff" id="<?php echo "category " . $cat->alias; ?>" >
+	<?php 
+	//--- Ausgabe des Bildes für die erste Seite die eines besitzt
 	if ($cat->articles != null) {
-		foreach ($cat->articles as $art) {
-			//--- Google maps anstadt Bild 
-			if (preg_match("#show_map#", $art->text) == 1) { ?>
+		foreach ($cat->articles as $art) { 				
+			if ($art->images != null) { //nur ausgeben wenn Bild vorhanden ist	?>
+			
+				<div class="page" id="<?php echo $cat->alias . ' _cont' ?>">
+					<h2 class="contacts"><?php echo $cat->cattitle; ?></h2>
+					<h3 class="contacts"><?php echo $art->title; ?></h3>
+					<img  class="artimg" src="<?php echo $art->images[1] ?>" alt=""/>
+				</div>
+		<?php	break;
+			}
+		} 
+	}
+	?>
+	</div>
+	<?php
+}
+
+//---------------------- Anzeige für GMaps Seite ----------
+else if (preg_match("#{show_map}#", $cat->text) == 1) { ?>
 				<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
 				<script>
 					function initialize() {
@@ -59,65 +120,15 @@ if (preg_match("#show_team#", $cat->text) != 1) { //überprüfe auf Spezialseite
 					      map: map,
 					      title: 'Hello World!'
 					  });
-					}
+				}
 					
 					google.maps.event.addDomListener(window, 'load', initialize);
-
     			</script> 
-				<div class="page gmap" id="<?php echo $cat->alias . ' ' . $art->alias; ?>">
-					<h3><?php echo $art->title; ?></h3>
+			<div class="category gmap" id="<?php echo "category " . $cat->alias; ?>" >
+				<div class="page gmap" id="<?php echo $cat->alias . ' _cont' ?>">
+					<h3><?php echo $cat->cattitle; ?></h3>
 					<div id="gmap"></div>
-				</div> <?php 
-			}
-			else if ($art->images != null) { //nur ausgeben wenn Bild vorhanden ist	?>
-				<div class="page" id="<?php echo $cat->alias . ' ' . $art->alias; ?>">
-					<h3><?php echo $art->title; ?></h3>
-					<img  class="artimg" src="<?php echo $art->images[0]; ?>" alt="" />
-				</div>
-		<?php }
-		} 
-	}
-	
-	//--- Ausgabe der zugehörigen Unterkategorien
-	foreach ($this->categories as $ucat) {		                               
-		if ($ucat->catparent == $cat->catid ) {
-			if ( $ucat->articles != null) { //nur wenn die Unterkategorie irgendeinen Inhalt hat
-				$content = false;
-				if ($ucat->images != null) { $content = true;} //hat selber Inhalt
-				foreach ($ucat->articles as $uart) { //prüfe Artikel auf Inhalt
-					if ($uart->images != null) { $content = true;}
-				}
-				if ($content == true) {
-					$this->curcat = $ucat;
-					echo $this->loadTemplate('category');
-				}
-			}
-		}
-	}?>
-	</div>
-	
-<?php }
-//---------------------- Anzeige für Mitarbeiterseite ----------
-else {?>
-
-	<div class="category staff" id="<?php echo "category " . $cat->alias; ?>" >
-	<?php 
-	//--- Ausgabe des Bildes für die erste Seite die eines besitzt
-	if ($cat->articles != null) {
-		foreach ($cat->articles as $art) { 				
-			if ($art->images != null) { //nur ausgeben wenn Bild vorhanden ist	?>
-			
-				<div class="page" id="<?php echo $cat->alias . ' _cont' ?>">
-					<h2 class="contacts"><?php echo $cat->cattitle; ?></h2>
-					<h3 class="contacts"><?php echo $art->title; ?></h3>
-					<img  class="artimg" src="<?php echo $art->images[1] ?>" alt=""/>
-				</div>
-		<?php	break;
-			}
-		} 
-	}
-	?>
-	</div>
-	<?php
+				</div> 
+			</div><?php 
 }
-?>
+
